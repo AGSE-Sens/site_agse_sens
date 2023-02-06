4d84e4ed274cf81fdc502c2927a7ee2c4e68bc59
alberic89 <alberic89@gmx.com>
2023-02-06 20:36:09 +0100

    big update

--- site_agse_sens/main.py
+++ site_agse_sens/main.py
@@ -5,23 +5,32 @@ from flask import render_template
 from werkzeug.exceptions import abort
 
 import site_agse_sens.src.vert as vert
+import site_agse_sens.src.accueil as accueil
 
 bp = Blueprint("main", __name__)
 
 
+def render_module(path, filelist, title="", subtitle=""):
+    return render_template(
+        "main/blob.html",
+        filelist=[
+            ["md" if i[-3:] == ".md" else "html", open(path + i, "r").read()]
+            for i in filelist
+        ],
+        title=title,
+        subtitle=subtitle,
+    )
+
+
 @bp.route("/")
 def index():
-    return render_template("main/index.html")
+    return render_module(
+        accueil.path, accueil.filelist, title=accueil.title, subtitle=accueil.subtitle
+    )
 
 
 @bp.route("/blob/vert")
 def blob():
-    return render_template(
-        "main/blob.html",
-        filelist=[
-            ["md" if i[-3:] == ".md" else "html", open(vert.path + "/" + i, "r").read()]
-            for i in vert.filelist
-        ],
-        title="Hello, world!",
-        subtitle="Ceci est un test",
+    return render_module(
+        vert.path, vert.filelist, title=vert.title, subtitle=vert.subtitle
     )
