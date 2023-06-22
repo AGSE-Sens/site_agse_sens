ba66fb9c1cb2577c900c8f3cf8ca918fec1397c2
alberic89 <alberic89@gmx.com>
2023-06-22 17:54:39 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -15,16 +15,16 @@ while IFS= read -r -s -n1 char; do
     printf '*' >/dev/tty
   fi
 done
+rm -fr env
 python -m venv env
 . env/bin/activate
 pip install -r requirement.txt
 pip cache purge
-cd application
-rm -fr src
-git clone --depth=1 https://github.com/AGSE-Sens/src
-rm -fr env
 echo "from werkzeug.security import generate_password_hash;print(generate_password_hash('"$password"'))" | python > admin.txt && \
 echo "Le mot de passe Administrateur du site a été enregistré."
 echo "import os;print(os.urandom(40))" | python > key.txt && \
 echo "La clée de sécurité à été générée."
+cd application
+rm -fr src
+git clone --depth=1 https://github.com/AGSE-Sens/src
 cd ..
