728ac934ff1a8fd984317cfd814bf24e59592f0c
alberic89 <alberic89@gmx.com>
2023-09-02 21:29:25 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,3 @@ python3 -m venv env
 . env/bin/activate
 python3 -m pip install --upgrade -r requirement.txt
 python3 -m pip cache purge
-cd application
-rm -fr src
-git clone --depth=1 https://github.com/AGSE-Sens/src
-cd ..
