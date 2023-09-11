0087d390714ab1efdb1c9990ee8d408891c58168
alberic89 <alberic89@gmx.com>
2023-09-11 18:52:56 +0200

    save more disk space

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 rm -fr env
 python3 -m venv env
 . env/bin/activate
-python3 -m pip install --upgrade -r requirement.txt
+export CLFAGS='-g0 -Wl -I/usr/include:/usr/local/include -L/usr/lib:/usr/local/lib'
+python3 -m pip install --upgrade -r requirement.txt --global-option=build_ext
 python3 -m pip cache purge
 echo "import os;open('key.txt','wb').write((os.urandom(40)))" | python3 && \
 echo "La clée de sécurité à été générée."
-git submodule update --remote
+git submodule update --remote --depth 1
 echo -e "#!/bin/bash
 . env/bin/activate
 export FLASK_DEBUG=ON
@@ -16,5 +17,5 @@ echo -e "#!/bin/bash
 . env/bin/activate
 python3 freeze.py
 deactivate" > freeze && chmod +x freeze
-./freeze
+#~ ./freeze
 deactivate
